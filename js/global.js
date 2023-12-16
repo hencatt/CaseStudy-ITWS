7373588d26fdd2344d459d2a2eca74cb55bf4aeb
hencat <142810213+hencatt@users.noreply.github.com>
2023-12-16 23:56:10 +0800

    Add files via upload

---
+++ js/global.js
@@ -0,0 +1,45 @@
+
+let modeRelax = false;
+let movement = 0;
+let stopwatch;
+let cardGroup = [
+["🐨", "🐱‍👤"], 
+["😒", "🐱‍🏍"],  
+["🎂", "🎉", "🌹", "🐱‍🐉"], 
+["🤢", "🐱‍🚀", "👀", "🎁"],
+["💋", "😤", "🐮", "🐲"]
+];
+
+let levelActual = 0;
+let levels = [
+    {
+        cards: cardGroup[0],
+        movementMax: 4
+    },
+    {
+        cards: cardGroup[0].concat(
+            cardGroup[1]),
+        movementMax: 10
+    },
+    {
+        cards: cardGroup[0].concat(
+            cardGroup[1], 
+            cardGroup[2]),
+        movementMax: 14
+    },
+    {
+        cards: cardGroup[0].concat(
+            cardGroup[1], 
+            cardGroup[2], 
+            cardGroup[3]),
+        movementMax: 25
+    },
+    {
+        cards: cardGroup[0].concat(
+            cardGroup[1], 
+            cardGroup[2], 
+            cardGroup[3], 
+            cardGroup[4]),
+        movementMax: 60
+    }
+];
